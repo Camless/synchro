7239716663eeead3ae49d134abd272507d2a4209
Carlos Morales III <morales22@outlook.com>
2020-09-18 03:01:43 -0500

    created xcode proj

---
+++ synchro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  synchro
+//
+//  Created by Carlos Morales III on 9/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ synchro/synchroApp.swift
@@ -0,0 +1,17 @@
+//
+//  synchroApp.swift
+//  synchro
+//
+//  Created by Carlos Morales III on 9/18/20.
+//
+
+import SwiftUI
+
+@main
+struct synchroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
